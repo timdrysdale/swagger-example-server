5c584d819ab0a3fb1d1eef1600ed223d9b1a300d
Tim Drysdale <timothy.d.drysdale@gmail.com>
2020-12-22 22:39:58 +0000

    Add debug prints

--- cmd/greeter/main.go
+++ cmd/greeter/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
 	"log"
+	"reflect"
 	"strings"
 
 	"github.com/dgrijalva/jwt-go"
@@ -28,7 +30,21 @@ func validateToken(name string, authenticate security.ScopedTokenAuthentication)
 
 // context values - requestScopedValues
 // https://medium.com/@cep21/how-to-correctly-use-context-context-in-go-1-7-8f2c0fafdf39
+// if adding omit_empty or other decorators, then improve reflection code e.g. https://stackoverflow.com/questions/40864840/how-to-get-the-json-field-names-of-a-struct-in-golang
+type Permission struct {
+	Host      string `json:"host"`
+	SessionID string `json:"session_id"`
+	Scope     string `json:"scope"`
+	Nbf       int64  `json:"nbf"`
+	Exp       int64  `json:"exp"`
+}
 
+func NewPermission() *Permission {
+	return &Permission{}
+}
+
+//https://swagger.io/docs/specification/2-0/authentication/
+//TODO - send correct error code, 401 / 403 rather than 500
 func ValidateHeader(bearerHeader string) (interface{}, error) {
 	bearerToken := strings.Split(bearerHeader, " ")[1]
 	claims := jwt.MapClaims{}
@@ -42,10 +58,31 @@ func ValidateHeader(bearerHeader string) (interface{}, error) {
 		log.Println(err.Error())
 		return nil, err
 	}
-	if token.Valid {
-		return claims["user"].(string), nil
+	if !token.Valid {
+		return nil, errors.New("invalid token")
+
 	}
-	return nil, errors.New("invalid token")
+
+	p := NewPermission()
+
+	fmt.Println(pretty(claims))
+
+	v := reflect.ValueOf(*p)
+	ty := v.Type()
+
+	for i := 0; i < v.NumField(); i++ {
+
+		k := ty.Field(i).Tag.Get("json")
+
+		if v, ok := claims[k]; ok {
+			fmt.Println(k, v)
+		} else {
+			return nil, fmt.Errorf("missing claim %s", k)
+		}
+
+	}
+	return p, nil
+
 }
 
 func main() {
@@ -72,13 +109,14 @@ func main() {
 	// set the Handler
 	api.GetGreetingHandler = operations.GetGreetingHandlerFunc(
 		func(params operations.GetGreetingParams, principal interface{}) middleware.Responder {
+			fmt.Println(pretty(params))
 			name := swag.StringValue(params.Name)
 			if name == "" {
 				name = "World"
 			}
 
 			greeting := fmt.Sprintf("Hello, %s!", name)
-			return operations.NewGetGreetingOK().WithPayload(greeting)
+			return operations.NewGetGreetingOK().WithPayload(greeting + pretty(principal))
 		})
 
 	//serve API
@@ -87,4 +125,14 @@ func main() {
 		log.Fatalln(err)
 	}
 
+}
+
+func pretty(t interface{}) string {
+
+	json, err := json.MarshalIndent(t, "", "\t")
+	if err != nil {
+		return ""
+	}
+
+	return string(json)
 }
